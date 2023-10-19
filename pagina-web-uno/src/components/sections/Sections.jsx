6c085c8645dae7f72f4ac1b539aa583899c84824
Lara Moyano <99929493+VicLara22@users.noreply.github.com>
2023-10-19 18:37:48 -0300

    [Frontend]: Add css conteo, react-boostrap col conteo. Add section conteo

--- pagina-web-uno/src/components/sections/Sections.jsx
+++ pagina-web-uno/src/components/sections/Sections.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nosotros from './nosotros/Nosotros'
 import Centro from './centro/Centro'
+import Conteo from './conteo/Conteo'
 
 function Sections() {
   return (
     <div>
         <Nosotros/>
         <Centro/>
+        <Conteo/>
     </div>
   )
 }
--- pagina-web-uno/src/components/sections/conteo/Conteo.jsx
+++ pagina-web-uno/src/components/sections/conteo/Conteo.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './conteo.css'
 
 function Conteo() {
   return (
-    <div>Conteo</div>
+    <div className='container-conteo'> 
+    <Container >
+      <Row md={4}>
+        <Col>
+          <h1>12</h1>
+          <p>AÑOS DE EXPERIENCIA</p>
+        </Col>
+        <Col xs={6}>
+          <h1>+20 MIL</h1>
+          <p>PACIENTES</p>
+        </Col>
+        <Col>
+          <h1>160</h1>
+          <p>M2 DE INSTALACIONES</p>
+        </Col>
+        <Col>
+          <h1>+70</h1>
+          <p>TRATAMIENTOS</p>
+        </Col>
+      </Row>
+    </Container>
+    </div>
   )
 }
 
