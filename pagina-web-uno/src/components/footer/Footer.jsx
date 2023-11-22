26e35a701a7a6cc6e115882f2f236c9f70695cea
Lara Moyano <99929493+VicLara22@users.noreply.github.com>
2023-11-22 12:43:42 -0300

    [FRONTEND]: Update files

--- pagina-web-uno/src/components/footer/Footer.jsx
+++ pagina-web-uno/src/components/footer/Footer.jsx
@@ -3,14 +3,64 @@ import './footer.css'
 import { BsWhatsapp } from 'react-icons/bs'
 import { BsInstagram } from 'react-icons/bs'
 import { BsFacebook } from 'react-icons/bs'
+import { Link } from 'react-scroll';
 
 function Footer() {
+
+    const links = [
+        {
+            id: '1',
+            title: 'Nosotros',
+            to: 'nosotros'
+
+        },
+        {
+            id: '2',
+            title: 'Centro',
+            to: 'centro'
+
+        },
+        {
+            id: '3',
+            title: 'Servicios',
+            to: 'servicios'
+        },
+        {
+            id: '4',
+            title: 'Contacto',
+            to: 'contacto'
+        },
+
+    ]
+
     return (
 
         <footer >
-            <div className='containerText'>
 
-                <h1>© Todos los derechos reservados</h1>
+            <div className='containerText'>
+                <div className="container-secciones">
+                    {
+                        links.map((t) => (
+                            <Link
+                            className="container-title-secciones "
+                                to={t.to}
+                                spy={true}
+                                smooth={true}
+                                offset={-50}
+                                duration={200}
+                                key={t.id}>
+                                {t.title}
+                            </Link>
+                        )
+                        )
+                    }
+                </div>
+            </div>
+            <div className='containerText'>
+                <div className="redes-sociales-icon">
+                    <BsInstagram className='instagram' />
+                    <BsFacebook className='facebook' />
+                </div>
 
                 <div className='whatsapp'>
                     <a
@@ -19,16 +69,12 @@ function Footer() {
                     >
                         <BsWhatsapp className='icon' />
                     </a>
-
                 </div>
+
                 <p className='msj'>Envianos un mensaje</p>
             </div>
-            <div>
-                <div className="redes-sociales-icon">
-                    <BsInstagram className='instagram'/>
-                    <BsFacebook className='facebook'/>
-                </div>
-            </div>
+            <h1>© Todos los derechos reservados</h1>
+
         </footer>
 
     )
